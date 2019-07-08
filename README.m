d9490a4a81c4c85d52cdcd7e05e0b65524b5927f
fabienchapuis <fabien-chapuis@netc.fr>
2019-07-08 09:00:31 +0000

    redaction du README avec ajout de la consigne

--- README.m
+++ README.m
@@ -1,1 +1,20 @@
-# dashboard_storm
+                                ### Projet Dashboard ##
+
+
+Client : Entraineur d'une équipe de foot.
+
+Je souhaite que vous développier une application sous forme de dashboard qui me permettrait de pouvoir savoir quel parent a 
+emmené des enfants le week-end en compétition.
+
+J'ai besoin d'un système d'utilisateur avec moi en tant qu'admin, 
+et les parents en tant que simple utilisateur.
+
+Les parents en arrivant sur le dashboard verrait mes notifiations de demande de conduite d'enfant 
+en compétition et pourraient répondre par "oui" ou par "non"
+Moi en tant qu'admin, j'aurais un suivi de la semaine en cours, 
+
+mais également un suivi par mois et un suivi par an sous forme de tableau/graphique/camembert ou autre de l'investissement des parents.
+Combien de fois tel parent a t'il conduit les enfant ?
+Je veux que ce soit pratique / fonctionnel / facile d'utilisation et que l'interface utilisateur soit bien pensée 
+(Les parents ne sont pas tous à l'aise avec internet ) 
+
